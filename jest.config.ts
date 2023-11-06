25006dd84b563056025a01c9f23fd043ac77b4a0
枫荷 <fenghe@linkmore.com>
2023-11-06 21:00:22 +0800

    feat(master): init project

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+      "^.+\\.tsx?$": "ts-jest" 
+  // process `*.tsx` files with `ts-jest`
+  },
+  "moduleNameMapper": {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+    '^@app/(.*)$': '<rootDir>/$1',
+  },
+  // moduleNameMapper: {
+  //     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+  //         "moduleNameMapper": {
+  //     "^.+\\.svg$": "jest-svg-transformer",
+  //     "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+  //   },
+  // },
+}
